fee6754163a9835a28d86bc6240612da25e76bf8
Eric <ericchufengzeng@163.com>
2019-02-28 20:41:53 +0800

    commi

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import sys
 import pygame as game
 from pygame.locals import *
-import sound
+#import sound
 
 game.init()
 game.mixer.init()
 
 c1 = game.mixer.Sound('C1.mp3')
-c1.set_volume(0.6)
+c1.set_volume(0.4)
 
 c2 = game.mixer.Sound('C2.mp3')
 c2.set_volume(0.6)
 
 c3 = game.mixer.Sound('C3.mp3')
+c3.set_volume(0.6)
+
+c4 = game.mixer.Sound('C4.mp3')
+c4.set_volume(0.6)
+
+c5 = game.mixer.Sound('C5.mp3')
+c5.set_volume(0.6)
+
+c6 = game.mixer.Sound('C6.mp3')
+c6.set_volume(0.6)
+
+c7 = game.mixer.Sound('C7.mp3')
+c7.set_volume(0.6)
+
+c8 = game.mixer.Sound('C8.mp3')
+c8.set_volume(0.6)
